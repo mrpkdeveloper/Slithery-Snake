59cedbd581abf373108188189256b75fedb5d915
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-07-24 13:53:24 +0530

    snake object created

--- game.js
+++ game.js
@@ -2,31 +2,43 @@ function init() {
     console.log("in init")
     canvas = document.getElementById("mycanvas")
     pen = canvas.getContext('2d')
-    W = canvas.width
-    H = canvas.height
-    rect = {
-        x: 20,
-        y: 20,
-        w: 40,
-        h: 40,
-        speed: 30,
+    W = H = canvas.width = canvas.height = 500
+    sw = 32
+
+    snake = {
+        init_len: 5,
+        color: "blue",
+        cells: [],
+        direction: "right",
+        create_snake: function () {
+            for (var i = this.init_len; i > 0; i--) {
+                this.cells.push({ x: i, y: 0 })
+            }
+        },
+        draw_snake: function () {
+            for (var i = 0; i < this.cells.length; i++) {
+                pen.fillRect(this.cells[i].x * sw, this.cells[i].y * sw, sw - 2, sw - 2)
+            }
+
+        }
     }
+
+    snake.create_snake()
+
 }
 
 
 function draw() {
     // console.log("in draw")
-    pen.clearRect(0, 0, W, H)
-    pen.fillStyle = 'red'
-    pen.fillRect(rect.x, rect.y, rect.w, rect.h)
+    snake.draw_snake()
 }
 
 function update() {
     // console.log("in update")
-    rect.x += rect.speed
-    if (rect.x > W - rect.w || rect.x < 0) {
-        rect.speed *= -1
-    }
+    // rect.x += rect.speed
+    // if (rect.x > W - rect.w || rect.x < 0) {
+    //     rect.speed *= -1
+    // }
 }
 
 function gameloop() {

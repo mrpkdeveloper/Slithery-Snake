b49ef6b09ee7dccee96e818ee4052cf5c304a015
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-07-25 13:42:57 +0530

    added movement to the snake

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ function init() {
 
     snake = {
         init_len: 5,
-        color: "blue",
+        color: "black",
         cells: [],
         direction: "right",
         create_snake: function () {
@@ -17,19 +17,66 @@ function init() {
         },
         draw_snake: function () {
             for (var i = 0; i < this.cells.length; i++) {
+                pen.fillStyle = this.color
                 pen.fillRect(this.cells[i].x * sw, this.cells[i].y * sw, sw - 2, sw - 2)
             }
 
+        },
+
+        update_snake: function () {
+            //update according to the direction
+            this.cells.pop()
+            var headx = this.cells[0].x
+            var heady = this.cells[0].y
+            var nextx, nexty
+            if (this.direction == "right") {
+                nextx = headx + 1
+                nexty = heady
+            }
+            else if (this.direction == "left") {
+                nextx = headx - 1
+                nexty = heady
+            }
+            else if (this.direction == "down") {
+                nextx = headx
+                nexty = heady + 1
+            }
+            else if (this.direction == "up") {
+                nextx = headx
+                nexty = heady - 1
+            }
+            this.cells.unshift({ x: nextx, y: nexty })
         }
     }
 
+
+    function keypressed(e) {
+        // console.log("key pressed", e.key)
+        if (e.key == "ArrowRight") {
+            snake.direction = 'right'
+        }
+        else if (e.key == "ArrowLeft") {
+            snake.direction = 'left'
+        }
+        else if (e.key == "ArrowUp") {
+            snake.direction = 'up'
+        }
+        else {
+            snake.direction = 'down'
+        }
+        console.log(snake.direction)
+    }
+
     snake.create_snake()
+
+    document.addEventListener('keydown', keypressed)
 
 }
 
 
 function draw() {
     // console.log("in draw")
+    pen.clearRect(0, 0, W, H)
     snake.draw_snake()
 }
 
@@ -39,6 +86,7 @@ function update() {
     // if (rect.x > W - rect.w || rect.x < 0) {
     //     rect.speed *= -1
     // }
+    snake.update_snake()
 }
 
 function gameloop() {

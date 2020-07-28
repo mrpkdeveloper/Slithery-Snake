cdec7083fd2e9c5c95052a069f1f386528cf6d34
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-07-28 23:19:52 +0530

    food added

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ function init() {
     pen = canvas.getContext('2d')
     W = H = canvas.width = canvas.height = 500
     sw = 32
+    food = randomfood()
 
     snake = {
         init_len: 5,
@@ -12,9 +13,11 @@ function init() {
         direction: "right",
         create_snake: function () {
             for (var i = this.init_len; i > 0; i--) {
-                this.cells.push({ x: i, y: 0 })
+                this.cells.push({ x: i, y: 1 })
             }
         },
+
+
         draw_snake: function () {
             for (var i = 0; i < this.cells.length; i++) {
                 pen.fillStyle = this.color
@@ -24,11 +27,25 @@ function init() {
         },
 
         update_snake: function () {
+            var headx = this.cells[0].x    //current head
+            var heady = this.cells[0].y    //current head
+
+            //this will check whether snake has eaten the food or not 
+            //and also increse the snake length
+            if (food.x == headx && food.y == heady) {
+                food = randomfood()
+            }
+
+
+
+
+
+
             //update according to the direction
-            this.cells.pop()
-            var headx = this.cells[0].x
-            var heady = this.cells[0].y
+            this.cells.pop()               //THIS WILL REMOVE THE LAST CELL
             var nextx, nexty
+
+            //logic for new head
             if (this.direction == "right") {
                 nextx = headx + 1
                 nexty = heady
@@ -76,19 +93,34 @@ function init() {
 
 function draw() {
     // console.log("in draw")
-    pen.clearRect(0, 0, W, H)
+    pen.clearRect(0, 0, W, H)  //erase previous frame
     snake.draw_snake()
+    pen.fillRect(food.x * sw, food.y * sw, sw, sw)
 }
 
 function update() {
     //check condition for walls
     // console.log("in update")
     // rect.x += rect.speed
-    if (snake.cells[0].x > 15 || snake.cells[0].x < 0 || snake.cells[0].y < 0 || snake.cells[0].y > 15) {
+    var lastx = Math.round(W / sw)
+    var lasty = Math.round(H / sw)
+
+    if (snake.cells[0].x > lastx || snake.cells[0].x < 0 || snake.cells[0].y < 0 || snake.cells[0].y > lasty) {
         clearInterval(game)
         window.alert("game over")
     }
     snake.update_snake()
+}
+
+function randomfood() {
+    var foodx = Math.round(Math.random() * (W - sw) / sw)
+    var foody = Math.round(Math.random() * (H - sw) / sw)
+    var food = {
+        x: foodx,
+        y: foody,
+        color: "red"
+    }
+    return food
 }
 
 function gameloop() {
